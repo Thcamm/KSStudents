a5438b0ec3da02388f5c82af2861c9116c0d9740
Thcamm <nguyenducthiencam2k5@gmail.com>
2026-01-22 15:08:00 +0700

    Step 3: Update sample project with Hello button

--- TrainingApp/App.tsx
+++ TrainingApp/App.tsx
@@ -1,44 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { View, Button, Alert, StyleSheet } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+const App = () => {
+  // Function for Step 3
+  const handleHello = () => {
+    Alert.alert("xin chào");
+  };
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+       {/* Button for Step 3 */}
+      <Button title="Show Hello" onPress={handleHello} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    padding: 20,
   },
 });
 
