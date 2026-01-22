0a82833a8430917c821e391d3ab8655b916ee028
Thcamm <nguyenducthiencam2k5@gmail.com>
2026-01-22 16:25:22 +0700

    Init Calculator

---
+++ TrainingApp/Caculator.js
@@ -0,0 +1,18 @@
+export const calculate = (sign, arg1, arg2) => {
+  switch (sign) {
+    case 'PLUS':
+      return 0;
+
+    case 'MINUS':
+      return 0;
+
+    case 'MULTIPLY':
+      return 0;
+
+    case 'DIVIDE':
+      return 0;
+
+    default:
+      return 0;
+  }
+};
